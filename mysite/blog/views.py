f9dc4ac7da06141f29af20d3644bd939b21f4a0d
rin-m <s2010048@klis.tsukuba.ac.jp>
2022-04-23 20:09:36 +0900

    ListViewを追加

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,8 @@
-#from django.shortcuts import render
 from django.views import generic
+from .models import Post # Postモデルをimport
 
 class IndexView(generic.TemplateView):
     template_name = 'blog/index.html'
+
+class PostListView(generic.ListView): # generic の ListViewクラスを継承
+    model = Post # 一覧表示させたいモデルを呼び出し
