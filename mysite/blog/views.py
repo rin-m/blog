903810c03c53eed4703e7b2600971279286ed3b4
rin-m <s2010048@klis.tsukuba.ac.jp>
2022-04-23 20:06:00 +0900

    クラスベース汎用ビューの設定

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,5 @@
 #from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views import generic
 
-class IndexView(TemplateView):
+class IndexView(generic.TemplateView):
     template_name = 'blog/index.html'
