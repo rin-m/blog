2c8a6c3184fb39f42d74b17b8daf654619c0ccdb
rin-m <s2010048@klis.tsukuba.ac.jp>
2022-04-27 12:52:47 +0900

    urls.py の修正

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('post_list', views.PostListView.as_view(), name='post_list'),
+    path('post_create', views.PostCreateView.as_view(), name='post_create'),
 ]
