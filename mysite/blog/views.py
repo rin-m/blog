3ccd6fc68bc54af658944d297d87257a11a85efd
rin-m <s2010048@klis.tsukuba.ac.jp>
2022-04-27 12:54:21 +0900

    views.py の修正

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,6 @@
 from django.views import generic
+from django.urls import reverse_lazy
+from .forms import PostCreateForm # forms.py で作ったクラスをimport
 from .models import Post # Postモデルをimport
 
 class IndexView(generic.TemplateView):
@@ -6,3 +8,8 @@ class IndexView(generic.TemplateView):
 
 class PostListView(generic.ListView): # generic の ListViewクラスを継承
     model = Post # 一覧表示させたいモデルを呼び出し
+
+class PostCreateView(generic.CreateView): # 追加
+    model = Post # 作成したい model を指定
+    form_class = PostCreateForm # 作成した form クラスを指定
+    success_url = reverse_lazy('blog:post_list') # 記事作成に成功した時のリダイレクト先を指定
