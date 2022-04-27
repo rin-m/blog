467348337440cfae1c166c3c9fef6b0a8cf97d82
rin-m <s2010048@klis.tsukuba.ac.jp>
2022-04-27 14:19:23 +0900

    views.py の修正

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,9 +3,6 @@ from django.urls import reverse_lazy
 from .forms import PostCreateForm # forms.py で作ったクラスをimport
 from .models import Post # Postモデルをimport
 
-class IndexView(generic.TemplateView):
-    template_name = 'blog/index.html'
-
 class PostListView(generic.ListView): # generic の ListViewクラスを継承
     model = Post # 一覧表示させたいモデルを呼び出し
 
