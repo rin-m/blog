644dbba365bc94abe48dcdefee194f81d705e9b9
rin-m <s2010048@klis.tsukuba.ac.jp>
2022-04-27 13:51:36 +0900

    views.py の修正

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -13,3 +13,14 @@ class PostCreateView(generic.CreateView): # 追加
     model = Post # 作成したい model を指定
     form_class = PostCreateForm # 作成した form クラスを指定
     success_url = reverse_lazy('blog:post_list') # 記事作成に成功した時のリダイレクト先を指定
+
+class PostDetailView(generic.DetailView): # 追加
+    model = Post  # pk(primary key)はurls.pyで指定しているのでここではmodelを呼び出すだけで済む
+    
+class PostUpdateView(generic.UpdateView): # 追加
+    model = Post
+    form_class = PostCreateForm # PostCreateFormをほぼそのまま活用できる
+    success_url = reverse_lazy('blog:post_detail')
+class PostDeleteView(generic.DeleteView): # 追加
+    model = Post
+    success_url = reverse_lazy('blog:post_list')
